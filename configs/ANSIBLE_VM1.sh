2ec9d676bd1fcd04037c69b4bd8759c6b514462d
predacristian <46676370+predacristian@users.noreply.github.com>
2019-09-10 18:14:26 +0300

    Update ANSIBLE_VM1.sh

--- configs/ANSIBLE_VM1.sh
+++ configs/ANSIBLE_VM1.sh
@@ -3,16 +3,17 @@
 yum install wget -y
 yum install vim -y
 yum install epel-release -y
-yum install pip-python -y
-pip install virtualenv -y
+yum install python-pip -y
+pip install virtualenv
 cd /home/vagrant
 virtualenv ansible
 source  ansible/bin/activate
+pip install ansible
+mkdir /home/vagrant/ansible/configs
 
-yum install ansible -y
 
 
 echo "[servers]
 192.168.56.101
 192.168.56.102
-192.168.56.103" > /etc/ansible/hosts
+192.168.56.103" > /ansible/configs/asd
