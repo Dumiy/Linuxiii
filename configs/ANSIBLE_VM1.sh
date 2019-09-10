b48785976dffda9f51a5b7ab7d4f84f696bb1685
Cristian Florin Preda <Cristian.Preda@endava.com>
2019-09-10 18:13:37 +0300

    all

--- configs/ANSIBLE_VM1.sh
+++ configs/ANSIBLE_VM1.sh
@@ -3,7 +3,15 @@
 yum install wget -y
 yum install vim -y
 yum install epel-release -y
+yum install pip-python -y
+pip install virtualenv -y
+cd /home/vagrant
+virtualenv ansible
+source  ansible/bin/activate
+
 yum install ansible -y
+
+
 echo "[servers]
 192.168.56.101
 192.168.56.102
--- configs/SSH_STUFF.sh
+++ configs/SSH_STUFF.sh
@@ -8,4 +8,14 @@ cp /shared_folder_JAT/id_rsa /home/vagrant/.ssh
 cp /shared_folder_JAT/id_rsa.pub /home/vagrant/.ssh
 cp /shared_folder_JAT/config /home/vagrant/.ssh
 
+chmod 700 /home/vagrant/.ssh
+chown vagrant:vagrant /home/vagrant/.ssh/authorized_keys
+chown vagrant:vagrant /home/vagrant/.ssh/id_rsa
+chown vagrant:vagrant /home/vagrant/.ssh/id_rsa.pub
+chown vagrant:vagrant /home/vagrant/.ssh/config
+chmod 644 /home/vagrant/.ssh/authorized_keys
+chmod 600 /home/vagrant/.ssh/id_rsa
+chmod 600 /home/vagrant/.ssh/id_rsa.pub
+chmod 600 /home/vagrant/.ssh/config
+
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCyy77UwX18dcWtKHLSC0Weh6Ceb8EJJLw9NbuyI1jc6qSodYbyWGfq5jsaPXz4oZS3cIIj2uWC9Y+Jt19rBZ0t4mxVMAR4hg4iM9VkrKgp1GdGpRvMxHwzf0/PvXjVlixIyC55tDcm2LGgLKlvxrSlQK5oANk9EGpFdBgVtwSDzHGqWWC2XhpPEkqT7YPomAi5OifhaqqrPn5PjOTC6k4dGJ9iu32D5gmGuPqvx2yppnVD3etMlyAZpmTVgYXNOZkBS55GR6lJIdclMPRovOstnSKIhVBPR7EfG8XzmwXWpmGeND7UN77m9iPmBI4dYwARiEnaBNh8x0YcrezQB3xPXXQDy6XpcOiWg7vB29QjuyIr9FIUeYQELaiZyCut5LXab7Fh9zOt+XBlixrPr2LM4OxQfW+e5nKAc4tM2RyQmyesh+G01DBSFFocO2VpWfoH9cOeF8g6PBh8DjDMMAXg9WyTaE/US0Li4NtQJrTfUnjCl/xOyU7D8BLJh5YmmUc= cpreda@EN1310279" >> /home/vagrant/.ssh/authorized_keys
