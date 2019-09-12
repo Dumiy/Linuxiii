5213b07f60e240bf1c9b553c9ef913c8d43e3270
Claudiu Cristian Dumitrascu <Claudiu.Dumitrascu@endava.com>
2019-09-12 09:49:07 +0300

    added jenkins and preq

--- configs/ANSIBLE_VM1.sh
+++ configs/ANSIBLE_VM1.sh
@@ -15,3 +15,12 @@ echo "[servers]
 192.168.56.101
 192.168.56.102
 192.168.56.103" > /home/vagrant/ansible/configs/inventory
+mkdir /home/vagrant/jenkins
+sudo yum install git -y
+cd /home/vagrant/jenkins
+git remote add  https://github.com/Dumiy/Linuxiii
+git pull origin master
+git checkout ansible
+cd configs/keys/ansible/jenkins
+ansible-galaxy install -r requirements.yml
+ansible-galaxy install avinash6784.maven
